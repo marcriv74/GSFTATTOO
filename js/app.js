9dd30ec810e5b5c85bca6df16f7c59788ab4842a
marcriv74 <marc.fer.riv@gmail.com>
2025-06-19 13:34:29 +0200

    efecto js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-var h =1;
+// ampliar imagen al hacer clic
+
+const imagenes = document.querySelectorAll('.galeria img');
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', (e) => {
+
+// Quitar la clase ampliada de todas las imágenes
+        imagenes.forEach(img => {
+            if (img !== imagen) {
+            img.classList.remove('ampliada');
+        }
+    });
+
+// Alternar la clase en la imagen clicada
+    imagen.classList.toggle('ampliada');
+    });
+
+//crear un efecto cuando pases por encima
+/*  
+imagen.addEventListener('mouseenter', () => {
+    if(!imagen.classList.contains("ampliar"))
+    imagen.classList.add('hover-activo');
+});
+
+imagen.addEventListener('mouseleave', () => {
+    imagen.classList.remove('hover-activo');
+    });*/ 
+});
